08fa6968750803c8322106d888e4c2d85a35a859
Alex Loembe <loembe.ak@gmail.com>
2023-12-18 19:51:32 -0500

    translations

--- medeo/contacts/routes.py
+++ medeo/contacts/routes.py
@@ -15,7 +15,7 @@ def add_language_code(endpoint, values):
 def pull_lang_code(endpoint, values):
     g.lang_code = values.pop('lang_code')
 
-
+@contacts.route("/contactus", methods=['GET','POST'])
 @contacts.route("/nouscontacter", methods=['GET','POST'])
 def contaceznous():
     form=ContactForm()
