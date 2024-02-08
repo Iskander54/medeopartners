bde8951bce6f05958d355761756d29c5f3b5a4b9
Alex Loembe <loembe.ak@gmail.com>
2024-02-07 20:42:52 -0500

    trad office et form

--- medeo/contacts/forms.py
+++ medeo/contacts/forms.py
@@ -1,14 +1,21 @@
-from flask_wtf import FlaskForm,RecaptchaField
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SubmitField, TextAreaField, EmailField
 from wtforms.validators import DataRequired, Length, Email
+from flask_babel import _, lazy_gettext as l
 
 
 class ContactForm(FlaskForm):
-    firstname = StringField(label=('Prénom'), validators=[DataRequired(),Length(min=2, max=30)])
-    lastname = StringField(label=('Nom'), validators=[DataRequired(),Length(min=2, max=30)])
-    phonenumber = StringField(label=('Téléphone'),validators=[Length(min=10, max=10)])
-    email = EmailField(label=('Email'),validators=[DataRequired(), Email()])
-    subject = StringField(label=('Sujet'),validators=[DataRequired(),Length(min=4, max=50)])
-    content = TextAreaField(label=('Message'), validators=[DataRequired(),Length(min=10, max=200)])
+    firstname = StringField(label=('Prénom'), validators=[DataRequired(), Length(
+        min=2, max=30)], render_kw={"placeholder": _("PRÉNOM")})
+    lastname = StringField(label=('Nom'), validators=[
+                           DataRequired(), Length(min=2, max=30)], render_kw={"placeholder": _("NOM")})
+    phonenumber = StringField(label=('Téléphone'), validators=[
+                              Length(min=10, max=10)], render_kw={"placeholder": _("TÉLÉPHONE")})
+    email = EmailField(label=('Email'), validators=[
+                       DataRequired(), Email()], render_kw={"placeholder": _("E-MAIL")})
+    subject = StringField(label=('Sujet'), validators=[
+                          DataRequired(), Length(min=4, max=50)], render_kw={"placeholder": _("SUJET")})
+    content = TextAreaField(label=('Message'), validators=[
+                            DataRequired(), Length(min=10, max=200)], render_kw={"placeholder": _("MESSAGE")})
     recaptcha = RecaptchaField()
     submit = SubmitField(label=('Envoyer'))
--- medeo/contacts/routes.py
+++ medeo/contacts/routes.py
@@ -1,31 +1,34 @@
 from flask import (render_template, url_for, flash,
-                   redirect, Blueprint,g)
+                   redirect, Blueprint, g)
 from flask_mail import Message
 from medeo import mail
-from flask_babel import _,refresh
+from flask_babel import _, refresh
 from medeo.contacts.forms import ContactForm
 
-contacts = Blueprint('contacts', __name__,url_prefix='/<lang_code>')
+contacts = Blueprint('contacts', __name__, url_prefix='/<lang_code>')
+
 
 @contacts.url_defaults
 def add_language_code(endpoint, values):
     values.setdefault('lang_code', g.lang_code)
 
+
 @contacts.url_value_preprocessor
 def pull_lang_code(endpoint, values):
     g.lang_code = values.pop('lang_code')
 
-@contacts.route("/contactus", methods=['GET','POST'])
-@contacts.route("/nouscontacter", methods=['GET','POST'])
+
+@contacts.route("/contactus", methods=['GET', 'POST'])
+@contacts.route("/nouscontacter", methods=['GET', 'POST'])
 def contaceznous():
-    form=ContactForm()
+    form = ContactForm()
     if form.validate_on_submit():
         with mail.connect() as conn:
             print(conn)
             # Mail sent to medeo to get info
             msg1 = Message(form.subject.data,
-                        sender='contact@medeo-partners.com',
-                        recipients=["contact@medeo-partners.com"])
+                           sender='contact@medeo-partners.com',
+                           recipients=["contact@medeo-partners.com"])
 
             msg1.body = """
             Hello Team,
@@ -35,10 +38,8 @@ def contaceznous():
             Phonenumber:{}
             Email: {}
             Message: {}
-            """.format(form.firstname.data,form.lastname.data,form.phonenumber.data,form.email.data,form.content.data)
+            """.format(form.firstname.data, form.lastname.data, form.phonenumber.data, form.email.data, form.content.data)
             conn.send(msg1)
             flash('Votre message a été delivré à la MEDEO Team !', 'success')
             return redirect(url_for('main.home'))
-    return render_template('nouscontacter.html',title='Contact',form=form)
-
-    
+    return render_template('nouscontacter.html', title='Contact', form=form)
