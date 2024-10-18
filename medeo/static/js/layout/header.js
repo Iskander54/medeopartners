9cc058ba3f4322887a13bbdeb9c20d9c54c2ae56
ah-4 <gv2ni.pro@gmail.com>
2024-10-18 12:08:19 +0200

    Ajout carousel Fix header

--- medeo/static/js/layout/header.js
+++ medeo/static/js/layout/header.js
@@ -1,12 +1,14 @@
 $(() => {
 
     // Toggle header background color and shadow on scroll
-    $(window).scroll(() => {
+    handleHeaderScroll = () => {
         if ($(this).scrollTop() > 0) {
             $('#header').addClass('bg-white shadow-sm');
         } else {
             $('#header').removeClass('bg-white shadow-sm');
         }
-    });
+    }
+    handleHeaderScroll();
+    $(window).scroll(handleHeaderScroll);
 
 })
