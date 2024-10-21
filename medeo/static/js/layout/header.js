8534f1b81dba9b8f5415149b5f451f1103fa226a
ah-4 <gv2ni.pro@gmail.com>
2024-10-21 16:31:09 +0200

    - Amélioration header et ajout des redirections pour "Notre expertise" - Ajout px pour containers

--- medeo/static/js/layout/header.js
+++ medeo/static/js/layout/header.js
@@ -11,4 +11,19 @@ $(() => {
     handleHeaderScroll();
     $(window).scroll(handleHeaderScroll);
 
+    $('#expertise_dropdown').on('click', function() {
+        let menu = $('#expertise_dropdown_menu');
+        menu.toggleClass('hidden');
+        // Set X and Y position of dropdown menu
+        menu = $('#expertise_dropdown_menu > div');
+        let rect = this.getBoundingClientRect();
+        menu.css('top', (rect.bottom + 8) + 'px');
+        menu.css('left', rect.left + 'px');
+    });
+
+    $('#expertise_dropdown_menu').on('click', function(e) {
+        $('#expertise_dropdown_menu').addClass('hidden');
+        e.stopPropagation();
+    });
+
 })
